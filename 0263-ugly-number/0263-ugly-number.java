bc42c9d6f9f6b217eae0af150f76ce09def7b9f1
HARSHIT SINGH$ <harshitsingh2004hardaspur@gmail.com>
2026-02-07 14:45:29 +0530

    Time: 1 ms (99.1%), Space: 42.3 MB (92.97%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.java
@@ -0,0 +1,31 @@
+// class Solution {
+//     public boolean isUgly(int n) {
+//         if(n<=0){
+//             return false;
+//         }
+//         while(n%2==0){
+//             n=n/2;
+//         }
+//         while(n%3==0){
+//             n=n/3;
+//         }
+//         while(n%5==0){
+//             n/=5;
+//         }
+//         return n==1;
+//         }
+//     }
+
+class Solution{
+    public boolean isUgly(int n){
+        if(n<=0){
+            return false;
+        }
+        for(int factor: new int[]{2,3,5}){
+            while(n%factor==0){
+                n/=factor;
+            }
+        }
+        return n==1;
+    }
+}
