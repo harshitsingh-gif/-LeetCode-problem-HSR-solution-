706123d4c5aa25e3641e1b4c97cd23584eaf731d
HARSHIT SINGH$ <harshitsingh2004hardaspur@gmail.com>
2026-02-13 22:57:02 +0530

    Time: 0 ms (100%), Space: 42.4 MB (51.93%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -1,12 +1,9 @@
 class Solution {
     public int trailingZeroes(int n) {
-          int count = 0;
-
-        while (n > 0) {
-            n = n / 5;
-            count += n;
+        int count=0;
+        for(int i=5;i<=n;i=i*5){
+            count = count+(n/i);
         }
-
-    return count;
+        return count;
     }
 }
