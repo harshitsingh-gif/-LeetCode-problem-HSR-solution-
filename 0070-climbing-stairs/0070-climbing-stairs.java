bcb338ced65016dd293e6aba07ef507dc6badcec
HARSHIT SINGH$ <harshitsingh2004hardaspur@gmail.com>
2026-02-17 01:43:50 +0530

    Time: 0 ms (100%), Space: 42 MB (87.56%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,14 +2,12 @@ class Solution {
     public int climbStairs(int n) {
         if(n==1) return 1;
         if(n==2) return 2;
-       int previ1= 1;
-       int previ2= 2;
-       int current=0;
-       for(int i=3;i<=n;i++){
-current= previ1+ previ2;
-previ1 =previ2;
-previ2 =current;
-       }
-       return previ2;
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
