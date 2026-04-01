c65025b065a6b0fb3242f74f6fa83c01971450ac
HARSHIT SINGH$ <harshitsingh2004hardaspur@gmail.com>
2026-04-01 22:52:31 +0530

    Time: 0 ms (100%), Space: 44 MB (81.86%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -4,7 +4,7 @@ class Solution {
         if(n  == 1) return 0;
         if(nums[0]> nums[1]) return 0;
         if(nums[n-1]>nums[n-2]) return n-1;
-        int left =1, right= n-2;
+         int left =1, right= n-2;
         while(left<= right){
             int mid = left+ (right-left)/2;
             if(nums[mid]>nums[mid-1] && nums[mid]> nums[mid+1]){
