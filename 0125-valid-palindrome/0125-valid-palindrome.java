06eb13552596364debd39cbc29d6870610332bb8
HARSHIT SINGH$ <harshitsingh2004hardaspur@gmail.com>
2026-04-23 17:31:44 +0530

    Time: 2 ms (99.37%), Space: 44.4 MB (66.72%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -6,7 +6,7 @@ class Solution {
             while(left<right && !Character.isLetterOrDigit(s.charAt(left))) left++;
             while(left<right && !Character.isLetterOrDigit(s.charAt(right))) right--;
         if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
-            return false;
+             return false;
         }
         left++;
         right--;
