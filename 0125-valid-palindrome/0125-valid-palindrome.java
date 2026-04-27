09e60fd7915db45e7d88508af089b68e2bfee59e
HARSHIT SINGH$ <harshitsingh2004hardaspur@gmail.com>
2026-04-27 21:15:44 +0530

    Time: 3 ms (60.18%), Space: 44.1 MB (84.38%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
         int left= 0;
-        int right = s.length()-1;
+         int right = s.length()-1;
           while(left<right){
              while(left<right && !Character.isLetterOrDigit(s.charAt(left))) left++;
              while(left<right && !Character.isLetterOrDigit(s.charAt(right))) right--;
