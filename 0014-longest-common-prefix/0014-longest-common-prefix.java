94304cd84c32fe97f21bde1a196d72d4ca5f6f9a
HARSHIT SINGH$ <harshitsingh2004hardaspur@gmail.com>
2026-05-02 21:01:07 +0530

    Time: 0 ms (100%), Space: 43.1 MB (82.37%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,7 +1,7 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
          if(strs==null||strs.length==0){
-             return "";
+              return "";
         }
         String prefix= strs[0];
         for(int i=1;i<strs.length;i++){
