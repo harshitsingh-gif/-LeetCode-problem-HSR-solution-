59eaf171eb5f5508d57f9af30dfd98ce9b41fc41
HARSHIT SINGH$ <harshitsingh2004hardaspur@gmail.com>
2026-05-07 17:42:21 +0530

    Time: 0 ms (100%), Space: 43.2 MB (19.85%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -3,7 +3,7 @@ class Solution {
         
         for(int i=0; i<haystack.length()-needle.length()+1;i++){
             if(haystack.charAt(i) == needle.charAt(0 )){
-                if(haystack.substring(i,needle.length()+i).equals(needle)){
+                 if(haystack.substring(i,needle.length()+i).equals(needle)){
                     return i;
                 }
             }
