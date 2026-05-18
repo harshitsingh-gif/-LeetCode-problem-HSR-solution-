14546b3274013a7b698207f20a7f745f9378c7fc
HARSHIT SINGH$ <harshitsingh2004hardaspur@gmail.com>
2026-05-18 21:01:34 +0530

    Time: 0 ms (100%), Space: 43.1 MB (18.13%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.java
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -3,7 +3,7 @@ class Solution {
         boolean seenZero = false;
 
         for (char c : s.toCharArray()) {
-            if (c == '0') {
+             if (c == '0') {
                 seenZero = true;
             } else if (seenZero) {
                 return false;
