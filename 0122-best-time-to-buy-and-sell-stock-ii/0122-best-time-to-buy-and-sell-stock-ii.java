94a5d0f9207b0f3739cc5c92580f8685a255cab2
HARSHIT SINGH$ <harshitsingh2004hardaspur@gmail.com>
2026-05-28 23:17:25 +0530

    Time: 0 ms (100%), Space: 46.6 MB (29.7%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -4,7 +4,7 @@ class Solution {
         
         for (int i = 1; i < prices.length; i++) {
             if (prices[i] > prices[i - 1]) {
-                profit += prices[i] - prices[i - 1];
+                 profit += prices[i] - prices[i - 1];
             }
         }
         
